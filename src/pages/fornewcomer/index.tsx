2ccab2828ad7b7f7bd7747948313d2dcd7e8d800
ibuki2003 <main@fuwa.dev>
2023-03-22 04:50:13 +0900

    move /fornewcomer into a directory

--- src/pages/fornewcomer/index.tsx
+++ src/pages/fornewcomer/index.tsx
@@ -1,6 +1,6 @@
 import Title from "components/title";
 import { useState } from "react";
-import styles from "./fornewcomer.module.scss";
+import styles from "./index.module.scss";
 
 const ForNewcomer = () => (
   <article className={styles.article}>
