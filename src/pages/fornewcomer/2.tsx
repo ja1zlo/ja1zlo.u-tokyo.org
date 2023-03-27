3322ed418d958bc56a514bb35d6d1014938c63f9
ibuki2003 <main@fuwa.dev>
2023-03-28 01:23:42 +0900

    update /fornewcomer/2

--- src/pages/fornewcomer/2.tsx
+++ src/pages/fornewcomer/2.tsx
@@ -1,74 +1,103 @@
+import React from "react";
 import Title from "components/title";
 import Link from "next/link";
 import styles from "./2.module.scss";
+import Image from "next/image";
+
+import ImagePaddle from "./paddle.webp";
+import ImageStraight from "./straight.webp";
 
 const ForNewcomer = () => (
   <article>
     <Title title="新歓立て看 解説" />
     <h1>立て看板の解説</h1>
 
-    <section className={styles.sign_sidebyside}>
-      <div className={styles.morse_text} role="figure">
-        <p>
-          -.-. --.-
-          <br />
-          -. . .--
-          <br />
-          -.-. ---
-          <br />
-          -- . .-. ...
-        </p>
-        <p>
-          -.. .
-          <br />
-          .--- .-
-          <br />
-          .---- --..
-          <br />
-          .-.. ---
-        </p>
-        <p>
-          .-- .
-          <br />
-          .-- .- -. -
-          <br />
-          -.-- --- ..-
-        </p>
-        <p>
-          .--. ... .<br />
-          -.-
-        </p>
-      </div>
-      <div className={styles.alpha_text}>
-        <p>
-          CQ
-          <br />
-          NEW
-          <br />
-          CO
-          <br />
-          MERS
-        </p>
-        <p>
-          DE
-          <br />
-          JA
-          <br />
-          1Z
-          <br />
-          LO
-        </p>
-        <p>
-          WE
-          <br />
-          WANT
-          <br />
-          YOU
-        </p>
-        <p>
-          PSE
-          <br />K
-        </p>
+    <section className={styles.sign_wrapper}>
+      <div className={styles.morse_sign}>
+        {(
+          [
+            [
+              [
+                ["C", "-.-."],
+                ["Q", "--.-"],
+              ],
+              [
+                ["N", "-."],
+                ["E", "."],
+                ["W", ".--"],
+              ],
+              [
+                ["C", "-.-."],
+                ["O", "---"],
+              ],
+              [
+                ["M", "--"],
+                ["E", "."],
+                ["R", ".-."],
+                ["S", "..."],
+              ],
+            ],
+            [
+              [
+                ["D", "-.."],
+                ["E", "."],
+              ],
+              [
+                ["J", ".---"],
+                ["A", ".-"],
+              ],
+              [
+                ["1", ".---"],
+                ["Z", "--.."],
+              ],
+              [
+                ["L", ".-.."],
+                ["O", "---"],
+              ],
+            ],
+            [
+              [
+                ["W", ".--"],
+                ["E", "."],
+              ],
+              [
+                ["W", ".--"],
+                ["A", ".-"],
+                ["N", "-."],
+                ["T", "-"],
+              ],
+              [
+                ["Y", "-.--"],
+                ["O", "---"],
+                ["U", "..-"],
+              ],
+            ],
+            [
+              [
+                ["P", ".--."],
+                ["S", "..."],
+                ["E", "."],
+              ],
+              [["K", "-.-"]],
+            ],
+          ] as [string, string][][][]
+        ).map((paragraph, i) => (
+          <p key={i}>
+            {paragraph.map((line, j) => (
+              <React.Fragment key={j}>
+                {line.map(([char, morse], k) => (
+                  <span key={k}>
+                    <ruby>
+                      {morse}
+                      <rt>{char}</rt>
+                    </ruby>{" "}
+                  </span>
+                ))}
+                <br />
+              </React.Fragment>
+            ))}
+          </p>
+        ))}
       </div>
     </section>
 
@@ -89,6 +118,29 @@ const ForNewcomer = () => (
         <dt>K</dt>
         <dd>どうぞ。送信をやめて受信状態に入ることを意味する。</dd>
       </dl>
+    </section>
+
+    <section>
+      <h2>モールス符号について</h2>
+      <p>
+        モールス符号は、電信のために開発された符号で、長短の点を組み合わせて文字や記号を表現するものです。
+        アマチュア無線では、音声の通信も行いますが、モールス符号を使った通信も現役で行われており、
+        東京大学アマチュア無線クラブにおいても、モールス符号を使った通信が行われています。
+      </p>
+
+      <p>
+        モールス信号は音声信号と比べ弱い信号でも聞き取ることができるため、より遠くの無線局との通信が可能になります。
+        過去にはヨーロッパやアメリカなどとも交信を行いました。
+      </p>
+
+      <p>
+        モールス符号は電鍵と呼ばれる装置を使って送信します。
+        以下の写真は、東大無線部が所有する電鍵です。
+      </p>
+      <div className={styles.keyer_images}>
+        <Image src={ImagePaddle} alt="パドル型電鍵" />
+        <Image src={ImageStraight} alt="縦振り型電鍵" />
+      </div>
     </section>
 
     <p>
