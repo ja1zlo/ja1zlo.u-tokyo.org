b9dfc0bd623ced6f715a8b083c8b4122194d89f9
ibuki2003 <main@fuwa.dev>
2024-05-14 15:39:22 -1000

    feat: migrate pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,25 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  markdown: {
+    remarkPlugins: [
+      myMarkdownPlugin ,
+    ],
+    extendDefualtPlugins: true,
+  },
 });
+
+function myMarkdownPlugin () {
+  return function (tree, file) {
+    const fm = file.data.astro.frontmatter;
+    if (!fm.layout) {
+      fm.layout = '@/layouts/markdown.astro'
+    }
+
+    if (!fm.title) {
+      const m = /^# *(.+)$/m.exec(file.value);
+      if (m) fm.title = m[1];
+    }
+  }
+}
